f030a95ec8eee95b534c769e91d37450a4ea61ad
571B235BA0A99F594A94096F8421767E <144430912+571B235BA0A99F594A94096F8421767E@users.noreply.github.com>
2025-03-13 15:40:01 +0530

    Update navbar.jsx

--- src/libio/navbar.jsx
+++ src/libio/navbar.jsx
@@ -44,7 +44,7 @@ import img1 from '../assets/cardimg/mainlogo.png'
    <li  onClick={()=>{navigate('/')}} className="hover:cursor-pointer hover:underline hover:text-blue-500">Home</li>
    <li onClick={()=>{navigate('/menu')}} className="hover:cursor-pointer  hover:underline hover:text-blue-500">Menu</li>
    <li onClick={()=>{navigate('/About')}} className="hover:cursor-pointer  hover:underline hover:text-blue-500">AboutUs</li>
-   <li onClick={()=>{navigate('/contactus')}}className="hover:cursor-pointer  hover:underline hover:text-blue-500">contactus</li>
+   <li onClick={()=>{navigate('/contactus')}}className="hover:cursor-pointer  hover:underline hover:text-blue-500">Contactus</li>
  </ol>
  <button onClick={nextpages} className=" hidden md:block  border-1 px-8 py-2 mr-10 rounded-full bg-white text-black hover:bg-blue-400 hover:text-white border-gray-400">
   Buy 
@@ -64,22 +64,22 @@ import img1 from '../assets/cardimg/mainlogo.png'
 {isOpen && (
  <div className="block xl:hidden ">
    <ol className="flex flex-col gap-6 justify-center items-center mt-5 text-xl">
-     <li onClick={()=>{navigate('/')}} className="focus:cursor-pointer hover:underline hover:text-blue-500">Home</li>
+     <li onClick={()=>{navigate('/')}} className="focus:cursor-pointer hover:underline hover:text-blue-500">HOME</li>
      <li  onClick={()=>{navigate('/menu')}} className="focus:cursor-pointer hover:underline hover:text-blue-500">
-      Menu
+  MENU
      </li>
      <li onClick={()=>{navigate('/About')}} className="focus:cursor-pointer hover:underline hover:text-blue-500">
-       AboutUs
+       ABOUT US
      </li>
      <li onClick={()=>{navigate('/contactus')}} className="focus:cursor-pointer hover:underline hover:text-blue-500">
-      Contactus
+     CONTACT US
      </li>
    </ol>
    <div className=" flex justify-center items-center  my-5 ">
      
      <button onClick={nextpages} className="border-2 px-8 py-2  rounded-full bg-white text-black border-gray-400 
       p-x-2 p-y-1  hover:bg-blue-400 hover:text-white">
-       Buy </button>
+       BUY </button>
    </div>
  </div>
 )}</div>
